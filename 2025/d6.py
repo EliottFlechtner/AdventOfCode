1f58c9f8caed1f84b23c5ebb2876930c97369cb4
Eliott Flechtner <eliott.flechtner@gmail.com>
2026-05-27 14:01:56 +0900

    fix

--- 2025/d6.py
+++ 2025/d6.py
@@ -1,31 +1,54 @@
-def solution_star1():
-    res = 0
+def solution_star1(lines):
+    results = []
 
+    operators = lines.pop()
+    for i, line in enumerate(lines):
+        numbers = list(map(int, line))
+        if i == 0:
+            results.extend(numbers)
+        else:
+            for k in range(len(numbers)):
+                op = operators[k]
+                print(f"Applying {op} to {results[k]} and {numbers[k]}")
+                if op == '+':
+                    results[k] += numbers[k]
+                elif op == '*':
+                    results[k] *= numbers[k]
+    return sum(results)
 
-    return res
+def reprocess_input(nums):
+    for i in range(len(nums)):
+        nums[i] = list(map(int, nums[i]))
 
-def solution_star2(list):
-    pass
+    results = []
+    for i, line in enumerate(nums):
+        for j in range(len(line)):
+            print(f"Processing line {i}, number {j}: {line[j]}")
+    return nums
+
+def solution_star2(lines):
+    results = []
+    operators = lines.pop()
+    print(reprocess_input(lines))
+
+    # for i, line in enumerate(lines):
+    #     numbers = list(map(int, line))
+    #     if i == 0:
+    #         results.extend(numbers)
+    #     else:
+    #         for k in range(len(numbers)):
+    #             op = operators[k]
+    #             print(f"Applying {op} to {results[k]} and {numbers[k]}")
+    #             if op == '+':
+    #                 results[k] += numbers[k]
+    #             elif op == '*':
+    #                 results[k] *= numbers[k]
+    return sum(results)
 
 
 if __name__ == "__main__":
+    lines = []
     with open("2025/input.txt") as f:
         lines = [l.strip().split() for l in f.readlines()]
-        print(lines)
         
-        results = []
-        operators = lines.pop()
-        print(operators)
-        for i, line in enumerate(lines):
-            numbers = list(map(int, line))
-            if i == 0:
-                results.extend(numbers)
-            else:
-                for k in range(len(numbers)):
-                    op = operators[k]
-                    print(f"Applying {op} to {results[k]} and {numbers[k]}")
-                    if op == '+':
-                        results[k] += numbers[k]
-                    elif op == '*':
-                        results[k] *= numbers[k]
-        print(sum(results))
+    print("Star 2:", solution_star2(lines))
---
+++ 2025/test.py
@@ -0,0 +1,6 @@
+N = 10
+S = N//2
+
+for i0 in range(2):
+    for i1 in range(S):
+        print(i0, i1, i0*S + i1)
